a0b13002d9ac036ae74b8b73ad99cabd21775da5
Aziz.Lachkar <aziz.lachkar@sinorfi.com>
2025-04-19 22:16:00 +0200

    fix commit

--- user-service/src/main/java/com/care4elders/userservice/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.care4elders.userservice.entity;
-
-public enum Role {
-    SOIGNANT,NORMAL_USER,ADMINISTRATOR
-}
--- userservice/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.care4elders.userservice.entity;
-
-public enum Role {
-    NURSE,USER,ADMINISTRATOR,DOCTOR
-}
