b14b0549e80c5d8b210b4ffc270c097bae082420
mohamed1312131 <mohamed1312131@gmail.com>
2025-05-04 22:13:08 +0100

    update chat front

--- careForEldersBack/chat/src/main/java/com/care4elders/chat/controller/ChatController.java
+++ careForEldersBack/chat/src/main/java/com/care4elders/chat/controller/ChatController.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/chats")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class ChatController {
 
--- careForEldersFront/src/app/pages/front-office/chat/chat.module.ts
+++ careForEldersFront/src/app/pages/front-office/chat/chat.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
+import { ChatAIComponent } from './chat-ai/chat-ai.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/material.module';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ChatAIComponent,
+    TestComponent
+  ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ChatModule { }
