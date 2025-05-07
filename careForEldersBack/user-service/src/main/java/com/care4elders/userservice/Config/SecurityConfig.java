2a8961e736197482c18d1d3c0c6b01e3f258c32d
mohamed1312131 <mohamed1312131@gmail.com>
2025-05-07 20:09:55 +0100

    u

--- careForEldersBack/user-service/src/main/java/com/care4elders/userservice/Config/SecurityConfig.java
+++ careForEldersBack/user-service/src/main/java/com/care4elders/userservice/Config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                         "/request-reset",
                         "/reset-password"
                 ).permitAll()
-                .requestMatchers(HttpMethod.GET, "/users/**").permitAll() // Add this line
+                .requestMatchers(HttpMethod.GET,HttpMethod.POST, "/users/**").permitAll() // Add this line
                 .anyRequest().authenticated()
         );
 
