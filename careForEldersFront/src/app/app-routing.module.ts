7a31360d233ca28e098a55a6a84f6ecccf9ffa5f
sarah-ammar1 <134142208+sarah-ammar1@users.noreply.github.com>
2025-05-07 22:13:33 +0100

    Update app-routing.module.ts

--- careForEldersFront/src/app/app-routing.module.ts
+++ careForEldersFront/src/app/app-routing.module.ts
@@ -2,54 +2,57 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
-import {PasswordResetComponent} from "./pages/authentication/password-reset/password-reset.component";
-import {ResetPasswordFormComponent} from "./pages/authentication/reset-password-form/reset-password-form.component";
-import {UsersComponent} from "./pages/front-office/user-service/user/user.component";
-import {MedicalRecordComponent} from "./pages/front-office/medical-record/medical-record/medical-record.component";
+
 
 const routes: Routes = [
-  // Public front office routes
+  // Front office routes
   {
-    path: "",
-    loadChildren: () => import("./pages/front-office/front-office.module").then((m) => m.FrontOfficeModule),
+    path: '',
+    loadChildren: () =>
+      import('./pages/front-office/front-office.module').then((m) => m.FrontOfficeModule),
   },
 
-  // Consolidated Admin routes
+  // Admin routes
   {
     path: 'admin',
-    component: BlankComponent,  // Main admin layout
+    component: BlankComponent,
     children: [
-      { path: "", redirectTo: "login", pathMatch: "full" },
       {
-        path: "login",
-        loadChildren: () => import("./pages/authentication/authentication.module").then((m) => m.AuthenticationModule),
+        path: '',
+        redirectTo: 'authentication/login',
+        pathMatch: 'full',
       },
       {
+        path: 'authentication',
+        loadChildren: () =>
+          import('./pages/authentication/authentication.module').then((m) => m.AuthenticationModule),
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    component: FullComponent,
+    
+    children: [
+      {
         path: 'dashboard',
-        component: FullComponent,  // Nested layout for dashboard
         loadChildren: () =>
           import('./pages/pages.module').then((m) => m.PagesModule),
       },
-      { path: 'front-office/user', component: UsersComponent }, // Route for User Administration
-      { path: 'front-office/medical-record', component: MedicalRecordComponent }, // Route for Medical Record
-
       {
         path: 'ui-components',
-        component: FullComponent,
         loadChildren: () =>
           import('./pages/ui-components/ui-components.module').then((m) => m.UicomponentsModule),
       },
       {
         path: 'extra',
-        component: FullComponent,
         loadChildren: () =>
           import('./pages/extra/extra.module').then((m) => m.ExtraModule),
       },
-      { path: "extra", loadChildren: () => import("./pages/extra/extra.module").then((m) => m.ExtraModule) },
     ],
   },
 
-  // Wildcard route
+  // Wildcard route for a 404 page
   {
     path: '**',
     redirectTo: '',
@@ -59,5 +62,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+
 })
 export class AppRoutingModule {}
