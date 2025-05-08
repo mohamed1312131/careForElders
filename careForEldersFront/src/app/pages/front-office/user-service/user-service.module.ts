c7412117393efd445a4791c5b39471a661dbba01
sarah <sarah.ammar@esprit.tn>
2025-05-08 18:20:44 +0100

    update

--- careForEldersFront/src/app/pages/front-office/user-service/user-service.module.ts
+++ careForEldersFront/src/app/pages/front-office/user-service/user-service.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { UsersComponent } from './user/user.component';
-=======
->>>>>>> origin/master
 import { UserServiceRoutingModule } from './user-service-routing.module';
 import { UserLayoutComponent } from './userProfile/user-layout/user-layout.component';
 import { ChatModule } from '../chat/chat.module';
 
 
 @NgModule({
-<<<<<<< HEAD
-  declarations: [UsersComponent],
-=======
   declarations: [UserLayoutComponent],
->>>>>>> origin/master
   imports: [
     CommonModule,
     UserServiceRoutingModule,
