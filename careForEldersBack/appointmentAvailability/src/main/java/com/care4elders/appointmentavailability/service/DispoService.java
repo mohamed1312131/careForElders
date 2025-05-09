15316272e30d77bb17ae260bc199b227f613164f
mohamed1312131 <mohamed1312131@gmail.com>
2025-05-09 16:16:20 +0100

    add xxx

--- careForEldersBack/appointmentAvailability/src/main/java/com/care4elders/appointmentavailability/service/DispoService.java
+++ careForEldersBack/appointmentAvailability/src/main/java/com/care4elders/appointmentavailability/service/DispoService.java
@@ -1,23 +1,28 @@
 package com.care4elders.appointmentavailability.service;
 
 import com.care4elders.appointmentavailability.entity.Disponibilite;
-
 import com.care4elders.appointmentavailability.repository.IDisponibiliteRepository;
-import lombok.AllArgsConstructor;
+import com.care4elders.appointmentavailability.dto.UserDTO;
 
+import jakarta.ws.rs.NotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-
 
 @Service
 @AllArgsConstructor
 public class DispoService implements IDispoService {
 
+    private final IDisponibiliteRepository disponibiliteRepo;
+    private final RestTemplate restTemplate;
 
-    private IDisponibiliteRepository disponibiliteRepo;
     @Override
     public Disponibilite createDisponibilite(Disponibilite dispo) {
+        validateDoctor(dispo.getDoctorId());
         return disponibiliteRepo.save(dispo);
     }
 
@@ -26,28 +31,27 @@ public class DispoService implements IDispoService {
         return disponibiliteRepo.findAll();
     }
 
-
     public List<Disponibilite> getDisponibilitesByDoctor(String doctorId) {
         return disponibiliteRepo.findByDoctorId(doctorId);
     }
 
-    /*
-    @Override
-    public List<Disponibilite> getDisponibilitesByMedecin(Long medecinId) {
-        return disponibiliteRepo.findByMedecinId(medecinId);
+    private void validateDoctor(String doctorId) {
+        try {
+            UserDTO doctor = restTemplate.getForObject(
+                    "http://USER-SERVICE/users/{id}",
+                    UserDTO.class,
+                    doctorId
+            );
+
+            if (doctor == null || !"DOCTOR".equalsIgnoreCase(doctor.getRole())) {
+                throw new NotFoundException("Doctor not found or invalid role");
+            }
+
+        } catch (HttpClientErrorException.NotFound ex) {
+            throw new NotFoundException("Doctor not found with ID: " + doctorId);
+        } catch (ResourceAccessException ex) {
+            throw new RuntimeException("User service unavailable");
+        }
     }
 
-    @Override
-    public Disponibilite updateDisponibilite(Long id, Disponibilite newDispo) {
-        return null;
-    }
-
-    @Override
-    public void deleteDisponibilite(Long id) {
-    }
-*/
-    //erride
- // public void deleteDisponibilite(Long id) {
-    //  disponibiliteRepo.deleteById(id);
-   //
 }
