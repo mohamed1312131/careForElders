8ffa503525851dd6d027497ff2050b458a23ebd7
sarah <sarah.ammar@esprit.tn>
2025-05-09 21:26:25 +0100

    front end of the posts blog/forum

--- careForEldersFront/src/app/pages/front-office/blog-forum/blog-forum.module.ts
+++ careForEldersFront/src/app/pages/front-office/blog-forum/blog-forum.module.ts
@@ -2,13 +2,47 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogForumRoutingModule } from './blog-forum-routing.module';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ConfirmDialogComponent } from "./confirm-dialog/confirm-dialog.component"
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PostListComponent, PostDetailComponent, PostFormComponent, ConfirmDialogComponent],
   imports: [
     CommonModule,
-    BlogForumRoutingModule
-  ]
+    ReactiveFormsModule,
+    BlogForumRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+  ],
 })
 export class BlogForumModule { }
