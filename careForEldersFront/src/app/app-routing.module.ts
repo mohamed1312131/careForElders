f001a8c8ccc4e294c0e0d6dae668dd118dc7aeca
sarah <sarah.ammar@esprit.tn>
2025-05-10 14:55:39 +0100

    changes merged

--- careForEldersFront/src/app/app-routing.module.ts
+++ careForEldersFront/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
+import { MedicalRecordComponent } from './pages/front-office/medical-record/medical-record/medical-record.component';
 
 
 const routes: Routes = [
