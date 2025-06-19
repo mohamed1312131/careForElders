03e197fdb0d3025592d4ab7b2c536dab80a9b98a
lyndabenrhaim <lbenrhaim@gmail.com>
2025-06-19 18:22:39 +0200

    some updates on the notification

--- careForEldersBack/notification/src/main/java/com/care4elders/notification/controller/NotificationController.java
+++ careForEldersBack/notification/src/main/java/com/care4elders/notification/controller/NotificationController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/notifications")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 public class NotificationController {
 
     @Autowired
